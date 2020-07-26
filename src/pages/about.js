08c28eb65ada537ea1ac301db078956cebb0b9bc
Bilal Tahir <bilal@MacBook-Pro.local>
2020-07-26 15:13:04 -0700

    cleanup

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import dogIllustration from "../images/dog-illustration.svg";
 
 function AboutPage() {
   return (
@@ -26,10 +25,6 @@ function AboutPage() {
             – Thomas Nagel
           </cite>
         </div>
-
-        <figure className="w-2/3 md:w-1/3">
-          <img alt="A dog relaxing" src={dogIllustration} />
-        </figure>
       </section>
     </Layout>
   );
