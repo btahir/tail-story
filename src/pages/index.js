71438be2d0e2e2e8301bab402011c7228653e401
Bilal Tahir <bilal@MacBook-Pro.local>
2020-07-29 21:24:38 -0700

    twitter try

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import TweetEmbed from 'react-tweet-embed';
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 function IndexPage() {
+
+  const TWITTER_DATA = {
+    topic: {
+      POSITIVE: ['1288557453723021312','1287899851921985538','1287816036201316353'],
+      NEGATIVE: ['1288253794309087232','1288567602344792071','1288196706706874369']
+    }
+  }
 
   return (
     <Layout>
@@ -11,9 +19,22 @@ function IndexPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Home"
       />
-      <h1>Sign Up for Premium Corgi Content</h1>
-
-  
+      <div className="text-center text-lg font-bold mb-8">Trending Topic</div>
+      <div className="flex justify-around">
+        <div className="w-1/3">
+          <div className="text-center text-lg font-bold text-gray-700 mb-4">Nays</div>
+          {TWITTER_DATA.topic.NEGATIVE.map(id => 
+            <TweetEmbed id={id} />
+          )}          
+        </div>
+        <div className="border-l-2 h-screen border-gray-400"></div>
+        <div className="w-1/3">
+          <div className="text-center text-lg font-bold text-gray-700 mb-4">Ayes</div>
+          {TWITTER_DATA.topic.POSITIVE.map(id => 
+            <TweetEmbed id={id} />
+          )}  
+        </div>
+      </div>
     </Layout>
   );
 }
