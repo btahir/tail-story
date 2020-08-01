cae9540179ba76870c932e565cf5792b9896c0bb
Bilal Tahir <bilal@MacBook-Pro.local>
2020-07-31 21:05:50 -0700

    add price plan

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,10 +1,12 @@
-const stripePromise = loadStripe("pk_test_8f6o9Uhjiz4mxCcNRJkcBZeL")
+import { loadStripe } from "gatsby-source-stripe";
+
+const stripePromise = loadStripe("pk_test_8f6o9Uhjiz4mxCcNRJkcBZeL");
 
 export const redirectToCheckout = async event => {
     event.preventDefault()
     const stripe = await stripePromise
     const { error } = await stripe.redirectToCheckout({
-        items: [{ sku: "sku_DjQJN2HJ1kkvI3", quantity: 1 }],
+        items: [{ plan: "price_1HBBvFJpKTxPdmIt0SK2MaaS", quantity: 1 }],
         successUrl: `http://localhost:8000/`,
         cancelUrl: `http://localhost:8000/`,
     })
