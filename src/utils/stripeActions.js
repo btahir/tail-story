3bf80dd61f5e253d78b951f21903583caa951e26
Bilal Tahir <bilal@MacBook-Pro.local>
2020-08-02 12:51:28 -0700

    fixup 3

--- src/utils/stripeActions.js
+++ src/utils/stripeActions.js
@@ -1,18 +1,3 @@
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-// export const createStripeCustomer = async (user) => {    
-//     // create a new customer in Stripe
-//     const customer = await stripe.customers.create({ email: user.email });
-//     console.log('customer', customer)
-
-//     await stripe.subscriptions.create({
-//         customer: customer.id,
-//         items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN }],
-//         return_url: 'https://example.com/account',
-//     });
-// }
-
-// import axios from 'axios';
 import firebase from "firebase";
 
 export const createStripeCustomer = (user) => {
