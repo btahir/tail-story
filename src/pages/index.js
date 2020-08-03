9f17997fda7cd22c59a9ce7fc51ffc280b557fb6
Bilal Tahir <bilal@MacBook-Pro.local>
2020-08-02 21:02:48 -0700

    style the front

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import { useAuth } from "gatsby-theme-firebase";
 import { manageStripeSubscription } from '../utils/stripeActions';
+import { getStripeSubscription } from '../utils/firebaseActions';
 
 function IndexPage() {
-  const { isLoggedIn, profile } = useAuth();
+  const { isLoading, isLoggedIn, profile } = useAuth();
+  const [stripePlan, setStripePlan] = useState(null);
+
+  useEffect(() => {
+    if (profile) {
+      getStripeSubscription(profile.uid).then(res => setStripePlan(res));
+    }
+  }, [profile])
+
+
   return (
     <Layout>
       <SEO
@@ -14,12 +24,17 @@ function IndexPage() {
         title="Home"
       />
       <div className="text-center text-2xl font-bold text-gray-800 tracking-wide" >{isLoggedIn ? `Welcome ${profile.displayName}` : 'Hello!'}</div>
-      <div className="flex justify-center mt-10 outline-none">
-        {isLoggedIn ?
-          <button onClick={() => manageStripeSubscription(profile.uid)} className="px-4 py-2 bg-teal-400 rounded text-gray-100 focus:outline-none">Manage Subscription</button>
-          : null
-        }
-      </div>
+      {isLoading ? null :
+        <div className="flex justify-center mt-10 outline-none">
+          {isLoggedIn ?
+            <div className="text-center">
+              <div className="mt-8 mb-16 text-gray-900 text-3xl">You are on the <span className="text-teal-400 font-semibold">{stripePlan}</span></div>
+              <button onClick={() => manageStripeSubscription(profile.uid)} className="text-xl px-4 py-2 bg-teal-400 rounded text-gray-100 focus:outline-none">Manage Subscription</button>
+            </div>
+            : null
+          }
+        </div>
+      }
     </Layout>
   );
 }
--- src/utils/firebaseActions.js
+++ src/utils/firebaseActions.js
@@ -10,12 +10,18 @@ export const createNewUser = (user) => {
     }
     // extract relevant data out of user object
     // store user in a user collection in Firestore
-    firestore.collection("users").doc(user.uid).set(firestoreUserData, {merge: true})
+    firestore.collection("users").doc(user.uid).set(firestoreUserData, { merge: true })
         .then(function () {
             return true;
         })
         .catch(function (error) {
             console.error("Error writing document: ", error);
             return false;
-    });
+        });
+}
+
+export const getStripeSubscription = async (id) => {
+    const plan = await firestore.collection("users").doc(id)
+        .get().then(doc => doc.data().stripeSubscriptionPlan)
+    return plan
 }
