5c70834da30016cbe1e3257ddd1b7e50954af5b9
Bilal Tahir <bilal@MacBook-Pro.local>
2020-08-08 14:07:59 -0700

    m

--- src/pages/submit.js
+++ src/pages/submit.js
@@ -49,10 +49,13 @@ function SubmitPage() {
                 Category
             </label>
             <div className="relative">
-                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                    <option>New Mexico</option>
-                    <option>Missouri</option>
-                    <option>Texas</option>
+                <select className="block appearance-none w-full border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                    <option>Fantasy</option>
+                    <option>Sci-Fi</option>
+                    <option>Mystery</option>
+                    <option>Romance</option>
+                    <option>Fan Fiction</option>
+                    <option>Other</option>
                 </select>
                 <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -60,7 +63,7 @@ function SubmitPage() {
             </div>
         </div>          
 
-          <button className="mt-4 px-4 py-2 text-sm font-bold text-white bg-gray-700 border-b-4 border-gray-800 rounded hover:border-gray-700 hover:bg-gray-600">
+          <button className="mt-4 px-6 py-2 text-md font-bold text-white bg-gray-700 border-b-4 border-gray-800 rounded hover:border-gray-700 hover:bg-gray-600">
             Submit
           </button>
         </form>
