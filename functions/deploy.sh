8d5fcf9b24ee2483b49810204a5047fb819c5c1c
Bilal Tahir <bilal@MacBook-Pro.local>
2020-09-08 23:07:14 -0700

    add stripe

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -1,8 +1,8 @@
 # careful not to save and publish live keys
-export STRIPE_SIGNING_UPD_SUB="whsec_VsCgys65PrFiMcmSbuAHOq813vgqRhv5"
-export STRIPE_TOKEN="sk_test_6EAXSQOCVAA2o0DPbm6wvHHD"
-export STRIPE_DEFAULT_PRICE_PLAN="price_1HBRCPJpKTxPdmItwPCtUFO3"
-export STRIPE_RETURN_URL="https://tail-story.netlify.app"
+export STRIPE_SIGNING_UPD_SUB="whsec_CTFcgTLw28qDplSZ2NITgifnpSV2Jept"
+export STRIPE_TOKEN="sk_test_51HPLspGcmYzIoqR7VzUXJWAzBP6AVeJfvMG2cyU6A5wLhqMOwKlfuSKUzXc3fas3OhGyvW31lPUqWtMgcdkSxqcg00otqSzc8I"
+export STRIPE_DEFAULT_PRICE_PLAN="price_1HPLwxGcmYzIoqR7ZvI7V0Jo"
+export STRIPE_RETURN_URL="https://project-job.netlify.app"
 
 firebase functions:config:set \
     stripe.upd_sub_signing=$STRIPE_SIGNING_UPD_SUB \
